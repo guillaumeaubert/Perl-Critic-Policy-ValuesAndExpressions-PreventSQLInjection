08d8426256b159d6dddc904bd276e8db80312e55
Guillaume Aubert <aubertg@cpan.org>
2013-12-05 13:15:21 -0800

    Released version 1.1.5.

--- lib/Perl/Critic/Policy/ValuesAndExpressions/PreventSQLInjection.pm
+++ lib/Perl/Critic/Policy/ValuesAndExpressions/PreventSQLInjection.pm
@@ -20,11 +20,11 @@ Perl::Critic::Policy::ValuesAndExpressions::PreventSQLInjection - Prevent SQL in
 
 =head1 VERSION
 
-Version 1.1.4
+Version 1.1.5
 
 =cut
 
-our $VERSION = '1.1.4';
+our $VERSION = '1.1.5';
 
 
 =head1 AFFILIATION
